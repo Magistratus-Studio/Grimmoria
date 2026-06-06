6eb0b35285d42ee7b8e7f042b808ae1f65d650ef
RodrigoAmaral <rodrigoamaral1705@gmail.com>
2026-06-06 20:06:22 -0300

    feat: movimentação por turno

--- grimmoria/Cenas/jogadorController.gd
+++ grimmoria/Cenas/jogadorController.gd
@@ -20,6 +20,8 @@ func _ready() -> void:
 	# await get_tree().process_frame
 	jogador.position = tileMap.map_to_local(SPAWNPLAYER)
 	inimigo.position = tileMap.map_to_local(SPAWNINIMIGO)
+	vizinhos = tileMap.get_surrounding_cells(tileMap.local_to_map(jogador.position))
+	destacarMovimento()
 
 
 #func _physics_process(delta: float) -> void:
@@ -28,24 +30,30 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT and not event.double_click:
 		var mousePos: Vector2 = event.position
-		print(tileMap.position)
-		print("local: ", tileMap.to_local(mousePos))
-		print("global: ", tileMap.to_global(mousePos))
-		print("mouse pos: ", mousePos)
+		var mausePosMapa: Vector2i = tileMap.local_to_map(tileMap.to_local(mousePos))
+		vizinhos = tileMap.get_surrounding_cells(tileMap.local_to_map(jogador.position))
+		if condicaoMovimento:
+			for vizinho in vizinhos:
+				if vizinho == mausePosMapa and tileMap.get_cell_tile_data(vizinho):
+					jogador.position = tileMap.map_to_local(mausePosMapa)
+					condicaoMovimento = not condicaoMovimento
+					destacarMovimento()
+	
+	if event is InputEventKey and event.is_pressed() and event.keycode == KEY_A:
+		destacarMovimento()
 
 
 func _on_turno_button_pressed() -> void:
 	condicaoMovimento = not condicaoMovimento
+	destacarMovimento()
 
 func destacarMovimento():
 	if condicaoMovimento:
-		vizinhos = tileMap.get_surrounding_cells(tileMap.local_to_map(jogador.position))
 		for vizinho in vizinhos:
 			var data = tileMap.get_cell_tile_data(vizinho)
 			if data:
 				destaqueLayer.set_cell(vizinho, 1, Vector2i(0,0))
 	else:
-		vizinhos = tileMap.get_surrounding_cells(tileMap.local_to_map(jogador.position))
 		for vizinho in vizinhos:
 			var data = tileMap.get_cell_tile_data(vizinho)
 			if data:
